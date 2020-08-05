288a3b62c073681402e633e6be4d21793815ca86
Ubiniti <piotr.puchacz@onet.pl>
2020-08-05 21:54:05 +0200

    Add app config, player, clock, text alignment

--- Application.py
+++ Application.py
@@ -15,6 +15,7 @@ class Application:
     def __init__(self, window_size):
         self.window = Window(window_size)
         self.__keys_pressed = []
+        self.clock = Clock()
 
     def on_key_up(self, key: int, mod: int):
         pass
@@ -28,6 +29,9 @@ class Application:
     def on_quit(self):
         sys.exit()
 
+    def on_update(self, detla_time: float):
+        pass
+
     def on_draw(self, window: 'Window'):
         pass
 
@@ -40,6 +44,9 @@ class Application:
 
             self.__keys_pressed = pygame.key.get_pressed()
             self.on_key_pressed(self.__keys_pressed)
+
+            self.clock.tick()
+            self.on_update(self.clock.get_delta_time())
 
             self.window.clear()
             self.on_draw(self.window)
@@ -60,7 +67,28 @@ class Window:
         drawable.draw(self)
 
 
-class Drawable:
+class Clock:
+    def __init__(self):
+        self.__pygame_clock = pygame.time.Clock()
+        self.__delta_time = 0.0
+
+    def tick(self) -> float:
+        self.__delta_time = self.__pygame_clock.tick()
+        return self.__delta_time
+
+    def get_delta_time(self) -> float:
+        return self.__delta_time
+
+    def get_fps(self):
+        return self.__pygame_clock.get_fps()
+
+
+class DrawableInterface:
+    def draw(self, window: Window):
+        pass
+
+
+class Drawable(DrawableInterface):
     def __init__(self, drawable):
         self.rect = drawable.get_rect()
         self.drawable = drawable
@@ -98,18 +126,47 @@ class Font:
 
 
 class TextLabel(Drawable):
+    ALIGN_LEFT = 0b1000
+    ALIGN_CENTER = 0b1100
+    ALIGN_RIGHT = 0b0100
+    VERTICAL_ALIGN_TOP = 0b0010
+    VERTICAL_ALIGN_MIDDLE = 0b0011
+    VERTICAL_ALIGN_BOTTOM = 0b0001
+
     def __init__(self, font: Font):
         self.position = (0, 0)
         self.font = font
         drawable = font.render("", True, WHITE, BLACK)
         Drawable.__init__(self, drawable)
+        self.alignment = self.ALIGN_LEFT | self.VERTICAL_ALIGN_MIDDLE
 
     def set_text(self, text: str):
         self.drawable = self.font.render(text, True, WHITE, BLACK)
         self.rect = self.drawable.get_rect()
-        self.rect.center = self.position
+        self.rect.bottomleft = self.position
 
     def set_position(self, pos: Vector2F):
         x, y = pos
         self.position = int(x), int(y)
         self.rect.center = self.position
+
+    def set_alignment(self, alignment_flags):
+        self.alignment = alignment_flags
+
+    def __set_rect_position(self, position: Vector2F):
+        if self.alignment == self.ALIGN_LEFT | self.VERTICAL_ALIGN_BOTTOM:
+            self.rect.bottomleft = position
+        elif self.alignment == self.ALIGN_LEFT | self.VERTICAL_ALIGN_MIDDLE:
+            self.rect.midleft = position
+        elif self.alignment == self.ALIGN_LEFT | self.VERTICAL_ALIGN_TOP:
+            self.rect.topleft = position
+        elif self.alignment == self.ALIGN_RIGHT | self.VERTICAL_ALIGN_BOTTOM:
+            self.rect.bottomleft = position
+        elif self.alignment == self.ALIGN_RIGHT | self.VERTICAL_ALIGN_MIDDLE:
+            self.rect.midleft = position
+        elif self.alignment == self.ALIGN_RIGHT | self.VERTICAL_ALIGN_TOP:
+            self.rect.topleft = position
+        elif self.alignment == self.ALIGN_CENTER | self.VERTICAL_ALIGN_MIDDLE:
+            self.rect.center = position
+        else:
+            raise Exception('TextLabel alignment value is not allowed')
--- MyGame.py
+++ MyGame.py
@@ -1,18 +1,69 @@
 from typing import List
-from Application import Application, Window, ImageSprite, Image, Key
+from Application import Application, Window, ImageSprite, Image, Key, DrawableInterface, TextLabel, Font, Clock
+
+RIGHT = 1
+LEFT = -1
+
+
+config = {
+    'image': {
+        'player': 'asset/ball.gif'
+    },
+    'font': {
+        'roboto': 'asset/Roboto-Regular.ttf'
+    },
+    'player': {
+        'running_speed': 1.0
+    },
+    'interface': {
+        'fps_counter_position': (200, 200)
+    }
+}
 
 
 class MyGame(Application):
     def __init__(self):
         Application.__init__(self, (800, 600))
-        ball_image = Image("asset/ball.gif")
-        self.ball_sprite = ImageSprite(ball_image)
+        self.player = Player()
+        self.fps_label = FpsLabel()
+        self.fps_label.set_position(
+            tuple(config['interface']['fps_counter_position'])
+        )
 
     def on_key_pressed(self, keys: List[bool]):
         if keys[Key.K_a]:
-            self.ball_sprite.move((-1.0, 0.0))
+            self.player.run(LEFT)
         if keys[Key.K_d]:
-            self.ball_sprite.move((1.0, 0.0))
+            self.player.run(RIGHT)
+
+    def on_update(self, detla_time: float):
+        self.fps_label.update(self.clock)
 
     def on_draw(self, window: 'Window'):
-        window.draw(self.ball_sprite)
+        self.player.draw(window)
+        self.fps_label.draw(window)
+
+
+class Player(DrawableInterface):
+    def __init__(self):
+        self.sprite = ImageSprite(
+            Image(config['image']['player'])
+        )
+        self.running_speed = float(config['player']['running_speed'])
+
+    def run(self, direction: int):
+        self.sprite.move((float(direction) * self.running_speed, 0.0))
+
+    def draw(self, window: Window):
+        self.sprite.draw(window)
+
+
+class FpsLabel(TextLabel):
+    def __init__(self):
+        self.font = Font(config['font']['roboto'])
+        TextLabel.__init__(self, self.font)
+        self.set_alignment(self.ALIGN_RIGHT | self.VERTICAL_ALIGN_MIDDLE)
+
+    def update(self, clock: Clock):
+        self.set_text(str(round(clock.get_fps())))
+
